a8f138862b8fb4fc70fd7e15ec036d05b327a93f
Samal <evgeniy.samal@mail.ru>
2023-03-10 21:15:18 +0300

    готовк использованию

---
+++ main.py
@@ -0,0 +1,33 @@
+def check_relation(net, first, second):
+    frends = set()
+    frends.add(first)
+    q = 0
+    while q < len(frends):
+        for item in net:
+            for j in item:
+                if j in frends:
+                    frends.add(item[0])
+                    frends.add(item[1])
+        q += 1
+    if second in frends:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    net = (
+        ("Ваня", "Лёша"), ("Лёша", "Катя"),
+        ("Ваня", "Катя"), ("Вова", "Катя"),
+        ("Лёша", "Лена"), ("Оля", "Петя"),
+        ("Стёпа", "Оля"), ("Оля", "Настя"),
+        ("Настя", "Дима"), ("Дима", "Маша"),
+    )
+
+    assert check_relation(net, "Петя", "Стёпа") is True
+    assert check_relation(net, "Маша", "Петя") is True
+    assert check_relation(net, "Ваня", "Дима") is False
+    assert check_relation(net, "Лёша", "Настя") is False
+    assert check_relation(net, "Стёпа", "Маша") is True
+    assert check_relation(net, "Лена", "Маша") is False
+    assert check_relation(net, "Вова", "Лена") is True
